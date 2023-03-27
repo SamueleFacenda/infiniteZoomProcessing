f1fbf99605ba5a2e032a62c365b95617b758f2e6
Samuele Facenda <samuele.facenda@gmail.com>
2023-03-27 15:20:11 +0200

    raindrop splash

---
+++ src/main/kotlin/examples/components/Splash.kt
@@ -0,0 +1,46 @@
+package examples.components
+
+import processing.core.PApplet
+import processing.core.PApplet.abs
+import processing.core.PApplet.pow
+
+class Splash(
+    radius: Float,
+    private val x: Float,
+    private val z: Float,
+    ) {
+    private val functionVariable = radius/2f
+    companion object{
+        const val SPAN = 10
+    }
+
+    private var start = 0
+    private var end = 0
+
+    private fun parabole(x: Float): Float{
+        return pow(abs(x) - functionVariable, 2f) / functionVariable + functionVariable
+    }
+
+    fun draw(sketch: PApplet){
+        sketch.stroke(0f, 50f, 200f)
+        sketch.noFill()
+
+        for (i in start..end) {
+            val y = parabole(i.toFloat())
+
+            sketch.pushMatrix()
+
+            sketch.translate(x, y, z)
+            sketch.rotateX(PApplet.PI/2f)
+            sketch.circle(0f,0f,i.toFloat())
+
+            sketch.popMatrix()
+        }
+
+        if(end > SPAN)
+            start++
+        if(parabole(end.toFloat()) > 0)
+            end++
+
+    }
+}
